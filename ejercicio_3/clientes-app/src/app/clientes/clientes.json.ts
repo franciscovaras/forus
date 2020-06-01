5dd3afada448b86794d3780a8f4b6b28d59b64ef
franciscovaras <franciscovaras03@hotmail.com>
2020-05-31 22:21:19 -0400

    update ejercicio 3

---
+++ ejercicio_3/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, productoNombre: 'Andrés', productoDescripcion: 'Guzmán', productoCantidad: 'profesor@bolsadeideas.com', productoUbicacion: '2017-12-11' },
+  { id: 2, productoNombre: 'Mr. John', productoDescripcion: 'Doe', productoCantidad: 'john.doe@gmail.com', productoUbicacion: '2017-11-11' },
+  { id: 3, productoNombre: 'Linus', productoDescripcion: 'Torvalds', productoCantidad: 'linus.torvalds@gmail.com', productoUbicacion: '2017-11-12' },
+  { id: 4, productoNombre: 'Rasmus', productoDescripcion: 'Lerdorf', productoCantidad: 'rasmus.lerdorf@gmail.com', productoUbicacion: '2017-11-13' },
+  { id: 5, productoNombre: 'Erich', productoDescripcion: 'Gamma', productoCantidad: 'erich.gamma@gmail.com', productoUbicacion: '2017-11-14' },
+  { id: 6, productoNombre: 'Richard', productoDescripcion: 'Helm', productoCantidad: 'richard.helm@gmail.com', productoUbicacion: '2017-11-15' },
+  { id: 7, productoNombre: 'Ralph', productoDescripcion: 'Johnson', productoCantidad: 'ralph.johnson@gmail.com', productoUbicacion: '2017-11-16' },
+  { id: 8, productoNombre: 'Bombasto', productoDescripcion: 'Vlissides', productoCantidad: 'bombasto.vlissides@gmail.com', productoUbicacion: '2017-11-17' },
+  { id: 9, productoNombre: 'Dr James', productoDescripcion: 'Gosling', productoCantidad: 'james.gosling@gmail.com', productoUbicacion: '2017-11-18' },
+  { id: 10, productoNombre: 'Magma', productoDescripcion: 'Lee', productoCantidad: 'magma.lee@gmail.com', productoUbicacion: '2017-11-19' },
+  { id: 11, productoNombre: 'Tornado', productoDescripcion: 'Roe', productoCantidad: 'tornado.roe@gmail.com', productoUbicacion: '2017-11-20' }
+];
