e383ccde79d705fb5e709c8999bddafec9123f36
franciscovaras <franciscovaras03@hotmail.com>
2020-06-10 23:10:09 -0400

    update backend api

--- ejercicio_2/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ProductoRestController.java
+++ ejercicio_2/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ProductoRestController.java
@@ -18,46 +18,53 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Producto;
 import com.bolsadeideas.springboot.backend.apirest.models.services.IProductoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
+@Api(tags = "producto")
 public class ProductoRestController {
-	
+
 	@Autowired
 	private IProductoService productoService;
-	
+
 	@GetMapping("/producto")
-	public List<Producto> index(){
+	public List<Producto> index() {
 		return productoService.findAll();
 	}
-	
+
 	@GetMapping("/producto/{id}")
 	public Producto show(@PathVariable Long id) {
 		return productoService.findById(id);
 	}
-	
+
 	@PostMapping("/producto")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Producto create(@RequestBody Producto cliente) {
 		return productoService.save(cliente);
 	}
-	
-	@PutMapping("clientes/{id}")
+
+	@PutMapping("producto/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Producto update(@RequestBody Producto producto, @PathVariable Long id) {
+
 		Producto productoActual = productoService.findById(id);
-		
+
 		productoActual.setProductoNombre(producto.getProductoNombre());
 		productoActual.setProductoDescripcion(producto.getProductoDescripcion());
 		productoActual.setProductoCantidad(producto.getProductoCantidad());
 		productoActual.setProductoUbicacion(producto.getProductoUbicacion());
-		
+
 		return productoService.save(productoActual);
 	}
-	
+
 	@DeleteMapping("/clientes/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(@PathVariable Long id){
+	public void delete(@PathVariable Long id) {
 		productoService.delete(id);
 	}
 }
